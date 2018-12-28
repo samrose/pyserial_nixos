b221469c7b1060c6fbd8c0159d971e4994db914e
samrose <samuel.rose@gmail.com>
2018-12-28 14:23:19 +0000

    pyserial on nixos

---
+++ requirements.nix
@@ -0,0 +1,127 @@
+# generated using pypi2nix tool (version: 2.0.0)
+# See more at: https://github.com/garbas/pypi2nix
+#
+# COMMAND:
+#   pypi2nix -V 3.7 -e pyserial
+#
+
+{ pkgs ? import <nixpkgs> {},
+  overrides ? ({ pkgs, python }: self: super: {})
+}:
+
+let
+
+  inherit (pkgs) makeWrapper;
+  inherit (pkgs.stdenv.lib) fix' extends inNixShell;
+
+  pythonPackages =
+  import "${toString pkgs.path}/pkgs/top-level/python-packages.nix" {
+    inherit pkgs;
+    inherit (pkgs) stdenv;
+    python = pkgs.python37;
+    # patching pip so it does not try to remove files when running nix-shell
+    overrides =
+      self: super: {
+        bootstrapped-pip = super.bootstrapped-pip.overrideDerivation (old: {
+          patchPhase = old.patchPhase + ''
+            if [ -e $out/${pkgs.python37.sitePackages}/pip/req/req_install.py ]; then
+              sed -i \
+                -e "s|paths_to_remove.remove(auto_confirm)|#paths_to_remove.remove(auto_confirm)|"  \
+                -e "s|self.uninstalled = paths_to_remove|#self.uninstalled = paths_to_remove|"  \
+                $out/${pkgs.python37.sitePackages}/pip/req/req_install.py
+            fi
+          '';
+        });
+      };
+  };
+
+  commonBuildInputs = [];
+  commonDoCheck = false;
+
+  withPackages = pkgs':
+    let
+      pkgs = builtins.removeAttrs pkgs' ["__unfix__"];
+      interpreterWithPackages = selectPkgsFn: pythonPackages.buildPythonPackage {
+        name = "python37-interpreter";
+        buildInputs = [ makeWrapper ] ++ (selectPkgsFn pkgs);
+        buildCommand = ''
+          mkdir -p $out/bin
+          ln -s ${pythonPackages.python.interpreter} \
+              $out/bin/${pythonPackages.python.executable}
+          for dep in ${builtins.concatStringsSep " "
+              (selectPkgsFn pkgs)}; do
+            if [ -d "$dep/bin" ]; then
+              for prog in "$dep/bin/"*; do
+                if [ -x "$prog" ] && [ -f "$prog" ]; then
+                  ln -s $prog $out/bin/`basename $prog`
+                fi
+              done
+            fi
+          done
+          for prog in "$out/bin/"*; do
+            wrapProgram "$prog" --prefix PYTHONPATH : "$PYTHONPATH"
+          done
+          pushd $out/bin
+          ln -s ${pythonPackages.python.executable} python
+          ln -s ${pythonPackages.python.executable} \
+              python3
+          popd
+        '';
+        passthru.interpreter = pythonPackages.python;
+      };
+
+      interpreter = interpreterWithPackages builtins.attrValues;
+    in {
+      __old = pythonPackages;
+      inherit interpreter;
+      inherit interpreterWithPackages;
+      mkDerivation = pythonPackages.buildPythonPackage;
+      packages = pkgs;
+      overrideDerivation = drv: f:
+        pythonPackages.buildPythonPackage (
+          drv.drvAttrs // f drv.drvAttrs // { meta = drv.meta; }
+        );
+      withPackages = pkgs'':
+        withPackages (pkgs // pkgs'');
+    };
+
+  python = withPackages {};
+
+  generated = self: {
+    "pyserial" = python.mkDerivation {
+      name = "pyserial-3.4";
+      src = pkgs.fetchurl {
+        url = "https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz";
+        sha256 = "6e2d401fdee0eab996cf734e67773a0143b932772ca8b42451440cfed942c627";
+      };
+      doCheck = commonDoCheck;
+      checkPhase = "";
+      installCheckPhase = "";
+      buildInputs = commonBuildInputs ++ [ ];
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://github.com/pyserial/pyserial";
+        license = licenses.bsdOriginal;
+        description = "Python Serial Port Extension";
+      };
+    };
+  };
+  localOverridesFile = ./requirements_override.nix;
+  localOverrides = import localOverridesFile { inherit pkgs python; };
+  commonOverrides = [
+    
+  ];
+  paramOverrides = [
+    (overrides { inherit pkgs python; })
+  ];
+  allOverrides =
+    (if (builtins.pathExists localOverridesFile)
+     then [localOverrides] else [] ) ++ commonOverrides ++ paramOverrides;
+
+in python.withPackages
+   (fix' (pkgs.lib.fold
+            extends
+            generated
+            allOverrides
+         )
+   )
